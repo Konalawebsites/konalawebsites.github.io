7e831dc29c96b3f05ab1d9634143f7da303ecfa3
Konalawebsites <atfitcad@gmail.com>
2023-11-04 14:32:12 +0200

    start

--- dist/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-var _client = _interopRequireDefault(require("react-dom/client"));
-var _App = _interopRequireDefault(require("./App"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-_client["default"].createRoot(document.getElementById('root')).render( /*#__PURE__*/React.createElement(_App["default"], null));
